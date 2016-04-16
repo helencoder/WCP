de5234b5140b17d480bab4d4fa8e34495d7c6ca1
helen <731873664@qq.com>
2016-04-16 18:45:14 +0800

    xdebug测试script

--- phpFiles/xdebug.php
+++ phpFiles/xdebug.php
@@ -2,5 +2,16 @@
 /**
  * Author: helen
  * CreateTime: 2016/4/15 11:44
- * description:
+ * description: xdebugµÄÊ¹ÓÃ
  */
+testXdebug();
+debug_backtrace();
+function testXdebug()
+{
+    requireFile();
+}
+
+function requireFile()
+{
+    require_once('abc.php');
+}
